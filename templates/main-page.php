db5ea0cef8281b76242f48452c18c488ba470e4b
farhankhd <98169192+farhankhd@users.noreply.github.com>
2022-02-02 15:23:24 -0500

    Update main-page.php

--- templates/main-page.php
+++ templates/main-page.php
@@ -1,12 +1,4 @@
 <?php
-header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
-?>
-
-<?php
     // Increase this version every time you change the style.css file to bust cached css
     $cssVersion = "1.0.9";
 ?>
